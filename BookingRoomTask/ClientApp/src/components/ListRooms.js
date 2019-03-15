d8586d5d27d184c6d29c0befcf12bad430ca19f6
Uvix Bag <baguvix88@outlook.com>
2019-03-15 21:56:28 +0300

    Added new react component for rooms list.

---
+++ BookingRoomTask/ClientApp/src/components/ListRooms.js
@@ -0,0 +1,56 @@
+﻿import React, { Component } from 'react';
+import { Glyphicon } from 'react-bootstrap';
+
+export class ListRooms extends Component {
+    displayName = ListRooms.name
+
+    static renderRoomsTable() {
+        let projectorIcon = <Glyphicon glyph='glyphicon glyphicon-facetime-video' />
+        let boardIcon = <Glyphicon glyph='glyphicon glyphicon-pencil' />
+
+        let roomsList = [
+            {
+                Id: 1,
+                Name: "2222",
+                NumSeat: 24,
+                HaveProjector: true,
+                HaveBoard: true,
+            },
+        ];
+
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Комната</th>
+                        <th>Количество мест</th>
+                        <th>Наличие проектора</th>
+                        <th>Наличие маркерной доски</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {roomsList.map(roomItem =>
+                        <tr key={roomItem.Id}>
+                            <td>{roomItem.Name}</td>
+                            <td>{roomItem.NumSeat}</td>
+                            <td>{roomItem.HaveProjector ? projectorIcon : "Отсутствует"}</td>
+                            <td>{roomItem.HaveBoard ? boardIcon : "Отсутствует"}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = ListRooms.renderRoomsTable();
+
+        return (
+            <div>
+                <h1>List rooms</h1>
+                <p>This component show all rooms list.</p>
+                {contents}
+            </div>
+        );
+    }
+}
--- BookingRoomTask/ClientApp/src/components/NavMenu.js
+++ BookingRoomTask/ClientApp/src/components/NavMenu.js
@@ -33,6 +33,11 @@ export class NavMenu extends Component {
                 <Glyphicon glyph='th-list' /> Fetch data
               </NavItem>
             </LinkContainer>
+            <LinkContainer to={'/list_rooms'}>
+              <NavItem>
+                <Glyphicon glyph='th-list' /> List rooms
+              </NavItem>
+            </LinkContainer>            
           </Nav>
         </Navbar.Collapse>
       </Navbar>
