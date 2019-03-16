3386088df6a2e4e9a8fff77e38994743bd07deb2
Uvix Bag <baguvix88@outlook.com>
2019-03-16 12:34:05 +0300

    Component ListRooms show all rooms list from data base.

--- BookingRoomTask/ClientApp/src/components/ListRooms.js
+++ BookingRoomTask/ClientApp/src/components/ListRooms.js
@@ -4,19 +4,22 @@ import { Glyphicon } from 'react-bootstrap';
 export class ListRooms extends Component {
     displayName = ListRooms.name
 
-    static renderRoomsTable() {
+    constructor() {
+        super();
+
+        this.state = { roomsList: [], loading: true };
+
+        fetch('api/Room')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ roomsList: data, loading: false });
+            });
+    }  
+
+    static renderRoomsTable(roomsList) {
         let projectorIcon = <Glyphicon glyph='glyphicon glyphicon-facetime-video' />
         let boardIcon = <Glyphicon glyph='glyphicon glyphicon-pencil' />
-
-        let roomsList = [
-            {
-                Id: 1,
-                Name: "2222",
-                NumSeat: 24,
-                HaveProjector: true,
-                HaveBoard: true,
-            },
-        ];
+        console.log(roomsList);
 
         return (
             <table className='table'>
@@ -30,11 +33,11 @@ export class ListRooms extends Component {
                 </thead>
                 <tbody>
                     {roomsList.map(roomItem =>
-                        <tr key={roomItem.Id}>
-                            <td>{roomItem.Name}</td>
-                            <td>{roomItem.NumSeat}</td>
-                            <td>{roomItem.HaveProjector ? projectorIcon : "Отсутствует"}</td>
-                            <td>{roomItem.HaveBoard ? boardIcon : "Отсутствует"}</td>
+                        <tr key={roomItem.id}>
+                            <td>{roomItem.name}</td>
+                            <td>{roomItem.numSeat}</td>
+                            <td>{roomItem.haveProjector ? projectorIcon : ''}</td>
+                            <td>{roomItem.haveBoard ? boardIcon : ''}</td>
                         </tr>
                     )}
                 </tbody>
@@ -43,7 +46,9 @@ export class ListRooms extends Component {
     }
 
     render() {
-        let contents = ListRooms.renderRoomsTable();
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : ListRooms.renderRoomsTable(this.state.roomsList);
 
         return (
             <div>
---
+++ BookingRoomTask/Models/RoomModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingRoomTask.Models
+{
+    public class RoomModel
+    {
+        public IEnumerable<Troom> GetAllRooms()
+        {
+            BookingRoomTaskContext db = new BookingRoomTaskContext();
+            return db.Troom.ToList();
+        }
+    }
+}
